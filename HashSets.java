452803c84bf94bbb11fbdefcb65affa1cddcc445
ratan-17 <rkmule2002@gmail.com>
2024-08-13 20:33:24 +0530

    ratan

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,28 @@
-public class HashSets{
+import java.util.HashSet;
+
+public class HashSets
+{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+
+
+public static void main(String[] args) 
+{
+    HashSet<Person> set=new HashSet<>();
+
+    set.add(new Person("Ratan", 22));
+    set.add(new Person("Abhi", 21));
+    set.add(new Person("shreyas", 18));
+
+
+    for(Object obj :set)
+    {
+        System.out.println(obj);
+    }
+    
+    
+}
+
+
+
+
 }
--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,25 @@
-public class LinkedHashSets{
-public static void main(String[]args){
+import java.util.*;
+
+public class LinkedHashSets
+{
+public static void main(String[]args)
+{
   // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+
+
+  LinkedHashSet <Object> set=new LinkedHashSet<>();
+
+  set.add("Java");
+  set.add(25);
+  set.add(null);
+  set.add(25);
+  set.add(56);
+
+  Iterator<Object> i=set.iterator();
+
+  while (i.hasNext()) 
+  {
+    System.out.println(i.next());
+  }
+}
 }
